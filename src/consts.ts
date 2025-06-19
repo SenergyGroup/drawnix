3d4fffd299350160605bafab96fbe1c17ad4861a
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2025-06-19 15:13:56 -0500

    Nav updated and about me

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Drawnix';
+export const SITE_DESCRIPTION = 'Follow my journey building a $50k digital business from scratch with AI and doodling. Learn alongside me and grab free resources for your own journey.';
