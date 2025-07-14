ae1d19f16bfc1bcdff21d1b9a7d5a5dee78e1a99
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2025-07-13 21:00:13 -0500

    Static to SSR web app

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 const site = process.env.RENDER_EXTERNAL_URL || 'http://localhost:4321';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
 	site: site,
 	integrations: [mdx(), sitemap(), react()],
+	adapter: node({
+    mode: 'standalone',
+  }),
 });
